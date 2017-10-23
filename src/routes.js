f1864e6f732573a698fe5914d08450b2bbb3eff0
lijingyanyan <yanlijing.tp@advantech.com.cn>
2017-10-23 10:14:02 +0800

    update

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,6 @@ import Index from './containers/index'
 import Demo from "./containers/demo";
 import Login from './containers/login';
 
-
 const routes = (
 <div style={{height:'100%'}}>
   <Route exact path='/' component={Login} />
