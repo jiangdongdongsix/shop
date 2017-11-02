8000b9f4daf8340b0debbf93a0c1d4ef411b4395
lijingyanyan <yanlijing.tp@advantech.com.cn>
2017-11-02 14:13:44 +0800

    update

---
+++ src/components/AddTable.js
@@ -0,0 +1,125 @@
+import React from 'react';
+import { Button,Input,Modal,Form } from 'antd';
+import './../styles/menu.css'
+const FormItem = Form.Item;
+const CreateForm = Form.create()(
+    (props) => {
+        const { visible, onCancel, onCreate, form } = props;
+        const { getFieldDecorator } = form;
+        const formItemLayout = {
+            labelCol: { span: 6 },
+            wrapperCol: { span: 12 },
+        };
+        return (
+            <Modal
+                visible={visible}
+                title="新增餐桌"
+                okText="确定"
+                onCancel={onCancel}
+                onOk={onCreate}
+            >
+                <Form layout='horizontal'>
+                    <FormItem label="餐桌编号" {...formItemLayout}>
+                        {getFieldDecorator('index', {
+                            rules: [{ required: true, message: '请输入餐桌编号' }],
+                        })(
+                            <Input />
+                        )}
+                    </FormItem>
+                    <FormItem label="餐桌类型" {...formItemLayout}>
+                        {getFieldDecorator('type', {
+                            rules: [{ required: true, message: '请输入餐桌类型' }],
+                        })(
+                            <Input />
+                        )}
+                    </FormItem>
+                    <FormItem label="餐桌所在区" {...formItemLayout}>
+                        {getFieldDecorator('area', {
+                            rules: [{ required: true, message: '请输入餐桌所在区' }],
+                        })(
+                            <Input />
+                        )}
+                    </FormItem>
+                    <FormItem label="餐桌最多容纳人数" {...formItemLayout}>
+                        {getFieldDecorator('pNumber', {
+                            rules: [{ required: true, message: '请输入餐桌最多容纳人数' }],
+                        })(
+                            <Input />
+                        )}
+                    </FormItem>
+                </Form>
+            </Modal>
+        );
+    }
+);
+
+const success = () => {
+    console.log('保存成功');
+};
+const error = () => {
+    console.log('保存失败');
+};
+export default class AddTable extends React.Component {
+    state = {
+        visible: false,
+    };
+    showModal = () => {
+        this.setState({ visible: true });
+    };
+    handleCancel = () => {
+        this.setState({ visible: false });
+    };
+    handleCreate = () => {
+        const form = this.form;
+        form.validateFields((err, values) => {
+            if (err) {
+                return;
+            }
+            console.log('Received values of form: ', values);
+            let tableNumber= {
+                name: values.index,
+                area:values.area,
+                tableTypeName: values.type,
+                eatMaxNumber:values.pNumber
+                };
+
+            console.log(tableNumber);
+            fetch('/restaurant/tableNumber', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(tableNumber)
+            }).then(function(response) {
+                return response.json();
+            }).then(function (jsonData) {
+                console.log("保存成功")
+                success();
+            }).catch(function () {
+                error();
+                console.log('出错了');
+            });
+            form.resetFields();
+            this.setState({ visible: false });
+        });
+    }
+    saveFormRef = (form) => {
+        this.form = form;
+    }
+    render() {
+        return (
+            <div>
+                <Button type="danger" icon="file-add" onClick={this.showModal}>
+                    <span style={{color:"white"}}>新增餐桌</span>
+                </Button>
+                <CreateForm
+                    ref={this.saveFormRef}
+                    visible={this.state.visible}
+                    onCancel={this.handleCancel}
+                    onCreate={this.handleCreate}
+                />
+            </div>
+        );
+    }
+}
--- src/components/TableSet.js
+++ src/components/TableSet.js
@@ -1,46 +1,47 @@
 import React from 'react';
 import { Table,Layout,Button,Row,Col,Input,Switch,Popconfirm,Icon,Modal,Form } from 'antd';
 import HeaderCustom from './HeaderCustom'
+import AddTable from './AddTable'
 import './../styles/tableset.css'
 const {Content} = Layout;
 const Search = Input.Search;
-const FormItem = Form.Item;
 
 export default class WaitVerify extends React.Component{
     constructor(props){
         super(props);
         this.state = {
             visible: false,
-            data:[],
-            idNum:1
+            table:[
+                {
+                    id:'',
+                    type:'',
+                    area:'',
+                    pNumber:''
+                }
+            ]
         };
         this.columns = [{
-            title: '菜品名称',
-            dataIndex: 'name',
-            width: '15%'
+            title: '餐桌编号',
+            dataIndex: 'id',
+            width: '18%'
         }, {
-            title: '菜品属性',
-            dataIndex: 'props',
-            width: '15%'
+            title: '餐桌类型',
+            dataIndex: 'type',
+            width: '18%'
         }, {
-            title: '价格',
-            dataIndex: 'Price',
-            width: '13%'
+            title: '餐桌所在区',
+            dataIndex: 'area',
+            width: '18%'
         },{
-            title: '会员价格',
-            dataIndex: 'VipPrice',
-            width: '13%'
-        },{
-            title: '描述',
-            dataIndex: 'describe',
+            title: '餐桌最多容纳人数',
+            dataIndex: 'pNumber',
             width: '23%'
         },{
-            title: '操作',
+            title: '',
             dataIndex: 'operation',
             render(){
                 return(
                     <span>
-                        <Switch defaultChecked={false}/>
                         <Icon type="edit" className="Menu-operation"/>
                         <Popconfirm title="确定删除?">
                             <Icon type="delete" className="Menu-operation"/>
@@ -51,35 +52,30 @@ export default class WaitVerify extends React.Component{
         }];
     }
     componentDidMount(){
-        fetch("/restaurant/menu?id=1")
+        let that = this;
+        fetch("/restaurant/tableNumber/tableNumbers")
             .then(function(response) {
                 return response.json();
             }).then(function (jsonData) {
             console.log(jsonData);
-
+            console.log(jsonData.tableNumber.length);
+            let len = jsonData.tableNumber.length;
+            let tableNumber = [];
+            for(let i=0;i<len;i++) {
+                tableNumber.push({
+                    id:jsonData.tableNumber[i].name,
+                    type:jsonData.tableNumber[i].tableType.tableTypeName,
+                    area:jsonData.tableNumber[i].area,
+                    pNumber:jsonData.tableNumber[i].tableType.eatMaxNumber
+                })
+            }
+            that.setState({table:tableNumber});
+            console.log(tableNumber);
         }).catch(function () {
             console.log('出错了');
         });
     }
 
-    /*模态框*/
-    showModal = () => {
-        this.setState({
-            visible: true,
-        });
-    };
-    handleOk = (e) => {
-        console.log(e);
-        this.setState({
-            visible: false,
-        });
-    };
-    handleCancel = (e) => {
-        console.log(e);
-        this.setState({
-            visible: false,
-        });
-    };
     render(){
         return (
             <Layout style={{backgroundColor:'white'}}>
@@ -107,9 +103,7 @@ export default class WaitVerify extends React.Component{
                             </Row>
                             <Row>
                                 <Col span={2}>
-                                    <Button type="danger" icon="file-add" onClick={this.showModal}>
-                                        <span style={{color:"white"}}>新增菜单</span>
-                                    </Button>
+                                    <AddTable/>
                                 </Col>
                                 <Col span={3}></Col>
                                 <Col span={2}>
@@ -117,7 +111,7 @@ export default class WaitVerify extends React.Component{
                                         <span style={{color:"white"}}>导入菜单</span>
                                     </Button>
                                 </Col>
-                                <Col span={9}></Col>
+                                <Col span={11}></Col>
                                 <Col span={6}>
                                     <Search
                                         placeholder="input search text"
@@ -127,28 +121,11 @@ export default class WaitVerify extends React.Component{
                             </Row>
                             <Row>
                                 <div className="Menu-table">
-                                    <Table columns={this.columns} dataSource={this.data} onChange={this.handleChange} />
+                                    <Table columns={this.columns} dataSource={this.state.table} onChange={this.handleChange} />
                                 </div>
                             </Row>
                         </Col>
                     </Row>
-                    <div>
-                        <Modal title="新增菜单" visible={this.state.visible}
-                               onOk={this.handleOk} onCancel={this.handleCancel}
-                        >
-                            <Form layout="vertical">
-                                <FormItem label="标题">
-
-                                </FormItem>
-                                <FormItem label="描述">
-
-                                </FormItem>
-                                <FormItem className="collection-create-form_last-form-item" style={{marginBottom: 0}}>
-
-                                </FormItem>
-                            </Form>
-                        </Modal>
-                    </div>
                 </Content>
             </Layout>
         )
