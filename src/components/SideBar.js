415d98019f13fb48e5ecd1f3957a47002d6d8e65
lijingyanyan <yanlijing.tp@advantech.com.cn>
2017-11-09 13:32:01 +0800

    update

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -28,14 +28,14 @@ export default class Side extends React.Component {
                         <h2 className="sideColor sideCenter">排队管理客户端</h2>
                         <Menu.Item key="1" className='iconColor'>
                             <Link to="/callnumber">
-                                <Icon type="bell"/>
+                                <i class="iconfont">&#xe616;</i>
                                 <span className="sideColor">叫号清桌</span>
                             </Link>
                         </Menu.Item>
 
                         <Menu.Item key="2">
                             <Link to="/verify">
-                                <i class="iconfont">&#xe65e;</i>
+                                <i class="iconfont">&#xe611;</i>
                                 <span className="sideColor">入场验证</span>
                             </Link>
                         </Menu.Item >
@@ -44,26 +44,26 @@ export default class Side extends React.Component {
                         </div>
                         <Menu.Item key="4" >
                             <Link to="/basicInfo">
-                                <i class="iconfont">&#xe603;</i>
+                                <i class="iconfont">&#xe607;</i>
                                 <span className="sideColor">基本信息设置</span>
                             </Link>
 
                         </Menu.Item>
                         <Menu.Item key="5">
                             <Link to="/tabletype">
-                                <i class="iconfont">&#xe624;</i>
+                                <i class="iconfont">&#xe613;</i>
                                 <span className="sideColor">桌类型设置</span>
                             </Link>
                         </Menu.Item>
                         <Menu.Item key="7">
                             <Link to="/tableset">
-                                <i class="iconfont">&#xe601;</i>
+                                <i class="iconfont">&#xe614;</i>
                             <span className="sideColor">门店桌位图及桌信息设置</span>
                             </Link>
                         </Menu.Item>
                         <Menu.Item key="8">
                             <Link to="/menu">
-                                <i class="iconfont">&#xe615;</i>
+                                <i class="iconfont">&#xe604;</i>
                             <span className="sideColor">菜单信息上传更新</span>
                             </Link>
                         </Menu.Item>
